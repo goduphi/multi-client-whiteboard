19ca867821c7c0a76f1923a2c9daaa823aad2c14
goduphi <sarkernadir.azmi@mavs.uta.edu>
2020-12-16 04:07:06 -0600

    Added functionality to synchronize two objects over the network

--- client.py
+++ client.py
@@ -1,5 +1,16 @@
+'''
+The pygame code was directly copied from Geeksforgeeks
+'''
+
 import socket
 import threading
+import pickle
+from utility import Vec2, Square
+import pygame
+
+# Global position of a player
+x = 200
+y = 200
 
 
 class ReceiveThread(threading.Thread):
@@ -8,24 +19,89 @@ class ReceiveThread(threading.Thread):
         self.sock = sock
 
     def run(self):
+        global x
+        global y
         while True:
-            print("Enter chat: ", end="")
-            message = input()
-            self.sock.send(bytes(message, 'utf-8'))
+            position = pickle.loads(self.sock.recv(2048))
+            x = position.x
+            y = position.y
 
 
-# intialize all of the network code
-# all the static code will be changed later
+# Initialize all of the network code
+# Note: All the static code will be changed later
 s = socket.socket()
 host = socket.gethostname()
 port = 1100
 s.connect((host, port))
 
+# This is where the game code starts
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+
+# Instantiate and start the thread to receive data from all connected clients
 user = ReceiveThread(s)
 user.start()
 
-while True:
-    print(s.recv(1024))
+# Helper to make sure I send data only when the object translates
+updated = False
 
-user.join
+square = Square(50, 50)
+vel = 10
+
+while True:
+    pygame.time.delay(10)
+
+    # iterate over the list of Event objects
+    # that was returned by pygame.event.get() method.
+    for event in pygame.event.get():
+
+        # if event object type is QUIT
+        # then quitting the pygame
+        # and program both.
+        if event.type == pygame.QUIT:
+            # it will make exit the while loop
+            run = False
+    # stores keys pressed
+    keys = pygame.key.get_pressed()
+
+    # if left arrow key is pressed
+    if keys[pygame.K_LEFT] and x > 0:
+        # decrement in x co-ordinate
+        x -= vel
+        updated = True
+
+        # if left arrow key is pressed
+    if keys[pygame.K_RIGHT] and x < 500 - square.x:
+        # increment in x co-ordinate
+        x += vel
+        updated = True
+
+        # if left arrow key is pressed
+    if keys[pygame.K_UP] and y > 0:
+        # decrement in y co-ordinate
+        y -= vel
+        updated = True
+
+        # if left arrow key is pressed
+    if keys[pygame.K_DOWN] and y < 500 - square.y:
+        # increment in y co-ordinate
+        y += vel
+        updated = True
+
+    if updated:
+        c = Vec2(x, y)
+        s.send(pickle.dumps(c))
+        updated = False
+
+    # completely fill the surface object
+    # with black colour
+    win.fill((0, 0, 0))
+
+    # drawing object on screen which is rectangle here
+    pygame.draw.rect(win, (255, 0, 0), (x, y, 50, 50))
+
+    # it refreshes the window
+    pygame.display.update()
+
+user.join()
 s.close()
--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 import socket
 import threading
+import pickle
+from utility import Vec2
 
 
 class ReceiveThread(threading.Thread):
-    def __init__(self, sock, sock_desp, clients):
+    def __init__(self, sock, sock_desp, clients, client_id):
         threading.Thread.__init__(self)
         self.sock = sock
         self.sock_desp = sock_desp
         self.clients = clients
         self.message = None
+        self.client_id = client_id
 
     def run(self):
         print("Waiting to receive a message...")
@@ -16,20 +19,22 @@ class ReceiveThread(threading.Thread):
             self.receive_message()
 
     def receive_message(self):
-        self.message = self.sock_desp.recv(1024)
-        print(f"Received message: {self.message}")
+        self.message = pickle.loads(self.sock_desp.recv(2048))
         self.send_to_all_clients()
 
     def send_to_all_clients(self):
         for client in self.clients:
-            client[0].send(self.message)
+            try:
+                client[0].send(pickle.dumps(self.message))
+            except Exception as e:
+                print(f"Send error: {e}")
 
 
 def notify_all_clients(addr, clients, client_id):
     for client in clients:
         if client_id != client[1]:
             message = "New client has joined the room: " + addr[0]
-            client[0].send(bytes(message, 'utf-8'))
+            client[0].send(pickle.dumps(message))
 
 
 # 0 as the third arg means TCP
@@ -44,15 +49,17 @@ clients = []
 threads = []
 client_id = 0
 
+current_position = Vec2(0, 0)
+
 while True:
     sock_des, addr = s.accept()
     print(f"New client joined: {addr}")
     clients.append((sock_des, client_id))
-    notify_all_clients(addr, clients, client_id)
-    client_id += 1
-    t = ReceiveThread(s, sock_des, clients)
+    #notify_all_clients(addr, clients, client_id)
+    t = ReceiveThread(s, sock_des, clients, client_id)
     t.start()
     threads.append(t)
+    client_id += 1
 
 for t in threads:
     t.join()
